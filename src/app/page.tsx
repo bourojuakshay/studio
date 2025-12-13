320558e188431fdc855a9c1561d67dd35d7d1143
BOUROJU AKSHAY <23eg106b12@anurag.edu.in>
2025-12-13 09:04:50 +0000

    it is so confusing to read it the words are MoodyO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,51 +103,60 @@ const STATIC_TRACKS = {
 };
 
 const MoodyOLoader = () => (
-  <div className="moody-loader-container">
-    <div className="loader">
-      <svg height="0" width="0" viewBox="0 0 64 64" className="absolute">
-        <defs className="s-xJBuHA073rTt" xmlns="http://www.w3.org/2000/svg">
-          <linearGradient className="s-xJBuHA073rTt" gradientUnits="userSpaceOnUse" y2="2" x2="0" y1="62" x1="0" id="b">
-            <stop className="s-xJBuHA073rTt" stopColor="#973BED"></stop>
-            <stop className="s-xJBuHA073rTt" stopColor="#007CFF" offset="1"></stop>
-          </linearGradient>
-          <linearGradient className="s-xJBuHA073rTt" gradientUnits="userSpaceOnUse" y2="0" x2="0" y1="64" x1="0" id="c">
-            <stop className="s-xJBuHA073rTt" stopColor="#FFC800"></stop>
-            <stop className="s-xJBuHA073rTt" stopColor="#F0F" offset="1"></stop>
-            <animateTransform repeatCount="indefinite" keySplines=".42,0,.58,1;.42,0,.58,1;.42,0,.58,1;.42,0,.58,1;.42,0,.58,1;.42,0,.58,1;.42,0,.58,1;.42,0,.58,1" keyTimes="0; 0.125; 0.25; 0.375; 0.5; 0.625; 0.75; 0.875; 1" dur="8s" values="0 32 32;-270 32 32;-270 32 32;-540 32 32;-540 32 32;-810 32 32;-810 32 32;-1080 32 32;-1080 32 32" type="rotate" attributeName="gradientTransform"></animateTransform>
-          </linearGradient>
-          <linearGradient className="s-xJBuHA073rTt" gradientUnits="userSpaceOnUse" y2="2" x2="0" y1="62" x1="0" id="d">
-            <stop className="s-xJBuHA073rTt" stopColor="#00E0ED"></stop>
-            <stop className="s-xJBuHA073rTt" stopColor="#00DA72" offset="1"></stop>
-          </linearGradient>
-        </defs>
-      </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="64" width="64" className="inline-block">
-        <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="8" stroke="url(#b)" d="M 4,60 V 4 h 24 v 56 h -24 z M 4,32 h 24" className="dash" id="m" pathLength="360"></path>
-      </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" style={{'--rotation-duration': '0ms', '--rotation-direction': 'normal'} as React.CSSProperties} viewBox="0 0 64 64" height="64" width="64" className="inline-block">
-        <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="10" stroke="url(#c)" d="M 32 32 m 0 -27 a 27 27 0 1 1 0 54 a 27 27 0 1 1 0 -54" className="spin" id="o" pathLength="360"></path>
-      </svg>
-       <svg xmlns="http://www.w3.org/2000/svg" fill="none" style={{'--rotation-duration': '0ms', '--rotation-direction': 'normal'} as React.CSSProperties} viewBox="0 0 64 64" height="64" width="64" className="inline-block">
-        <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="8" stroke="url(#c)" d="M 32, 60 C 17,60 10,48 10,32 C 10,16 17,4 32,4 C 47,4 54,16 54,32 C 54,48 47,60 32,60 z" className="dash" id="o2" pathLength="360"></path>
-      </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" style={{'--rotation-duration': '0ms', '--rotation-direction': 'normal'} as React.CSSProperties} viewBox="0 0 64 64" height="64" width="64" className="inline-block">
-        <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="8" stroke="url(#d)" d="M 60,60 V 4 L 32,32 l 28,28" className="dash" id="d_letter" pathLength="360"></path>
-      </svg>
-       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="64" width="64" className="inline-block">
-        <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="8" stroke="url(#b)" d="M 54.722656,3.9726563 A 2.0002,2.0002 0 0 0 54.941406,4 h 5.007813 C 58.955121,17.046124 49.099667,27.677057 36.121094,29.580078 a 2.0002,2.0002 0 0 0 -1.708985,1.978516 V 60 H 29.587891 V 31.558594 A 2.0002,2.0002 0 0 0 27.878906,29.580078 C 14.900333,27.677057 5.0448787,17.046124 4.0507812,4 H 9.28125 c 1.231666,11.63657 10.984383,20.554048 22.6875,20.734375 a 2.0002,2.0002 0 0 0 0.02344,0 c 11.806958,0.04283 21.70649,-9.003371 22.730469,-20.7617187 z" className="dash" id="y" pathLength="360"></path>
-      </svg>
+    <div className="moody-loader-container">
+      <div className="loader">
+        <svg height="0" width="0" viewBox="0 0 64 64" className="absolute">
+          <defs className="s-xJBuHA073rTt" xmlns="http://www.w3.org/2000/svg">
+            <linearGradient className="s-xJBuHA073rTt" gradientUnits="userSpaceOnUse" y2="2" x2="0" y1="62" x1="0" id="b">
+              <stop className="s-xJBuHA073rTt" stopColor="#973BED"></stop>
+              <stop className="s-xJBuHA073rTt" stopColor="#007CFF" offset="1"></stop>
+            </linearGradient>
+            <linearGradient className="s-xJBuHA073rTt" gradientUnits="userSpaceOnUse" y2="0" x2="0" y1="64" x1="0" id="c">
+              <stop className="s-xJBuHA073rTt" stopColor="#FFC800"></stop>
+              <stop className="s-xJBuHA073rTt" stopColor="#F0F" offset="1"></stop>
+              <animateTransform repeatCount="indefinite" keySplines=".42,0,.58,1;.42,0,.58,1;.42,0,.58,1;.42,0,.58,1;.42,0,.58,1;.42,0,.58,1;.42,0,.58,1;.42,0,.58,1" keyTimes="0; 0.125; 0.25; 0.375; 0.5; 0.625; 0.75; 0.875; 1" dur="8s" values="0 32 32;-270 32 32;-270 32 32;-540 32 32;-540 32 32;-810 32 32;-810 32 32;-1080 32 32;-1080 32 32" type="rotate" attributeName="gradientTransform"></animateTransform>
+            </linearGradient>
+            <linearGradient className="s-xJBuHA073rTt" gradientUnits="userSpaceOnUse" y2="2" x2="0" y1="62" x1="0" id="d">
+              <stop className="s-xJBuHA073rTt" stopColor="#00E0ED"></stop>
+              <stop className="s-xJBuHA073rTt" stopColor="#00DA72" offset="1"></stop>
+            </linearGradient>
+          </defs>
+        </svg>
+        {/* M */}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="64" width="64" className="inline-block">
+          <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="8" stroke="url(#b)" d="M 8 60 V 4 L 32 32 L 56 4 V 60" className="dash" pathLength="360"></path>
+        </svg>
+        {/* O */}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" style={{'--rotation-duration': '0ms', '--rotation-direction': 'normal'} as React.CSSProperties} viewBox="0 0 64 64" height="64" width="64" className="inline-block">
+          <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="8" stroke="url(#c)" d="M 32, 60 C 17,60 10,48 10,32 C 10,16 17,4 32,4 C 47,4 54,16 54,32 C 54,48 47,60 32,60 z" className="dash" pathLength="360"></path>
+        </svg>
+        {/* O */}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" style={{'--rotation-duration': '0ms', '--rotation-direction': 'normal'} as React.CSSProperties} viewBox="0 0 64 64" height="64" width="64" className="inline-block">
+          <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="8" stroke="url(#c)" d="M 32, 60 C 17,60 10,48 10,32 C 10,16 17,4 32,4 C 47,4 54,16 54,32 C 54,48 47,60 32,60 z" className="dash" pathLength="360"></path>
+        </svg>
+        {/* D */}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="64" width="64" className="inline-block">
+          <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="8" stroke="url(#d)" d="M 12 4 V 60 M 12 32 C 12 16 32 16 48 32 C 64 48 12 48 12 32" className="dash" pathLength="360"></path>
+        </svg>
+        {/* Y */}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="64" width="64" className="inline-block">
+          <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="8" stroke="url(#b)" d="M 12 4 L 32 32 L 52 4 M 32 32 V 60" className="dash" pathLength="360"></path>
+        </svg>
+        {/* O */}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" style={{'--rotation-duration': '0ms', '--rotation-direction': 'normal'} as React.CSSProperties} viewBox="0 0 64 64" height="64" width="64" className="inline-block">
+            <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="10" stroke="url(#c)" d="M 32 32 m 0 -27 a 27 27 0 1 1 0 54 a 27 27 0 1 1 0 -54" className="spin" id="o" pathLength="360"></path>
+        </svg>
+      </div>
+      <motion.p
+        className="intro-subtitle"
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0, transition: { duration: 0.8, delay: 0.8 } }}
+      >
+        AI-Generated Soundscapes
+      </motion.p>
     </div>
-    <motion.p
-      className="intro-subtitle"
-      initial={{ opacity: 0, y: 10 }}
-      animate={{ opacity: 1, y: 0, transition: { duration: 0.8, delay: 0.8 } }}
-    >
-      AI-Generated Soundscapes
-    </motion.p>
-  </div>
-);
-
+  );
+  
 
 const InteractiveCard = ({ moodKey, emoji, title, onClick, style = {} }) => {
   return (
