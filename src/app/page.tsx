ad1683c4944f16f5a5961684441b39f76dfab14d
BOUROJU AKSHAY <23eg106b12@anurag.edu.in>
2025-12-13 14:04:54 +0000

    in Moodyo d is looking bad make it look like d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ const MoodyOLoader = ({ isExiting }: { isExiting: boolean }) => {
 
   return (
     <div className="moody-loader-container">
-      <div className={cn("loader items-center", { 'exit-animation': isExiting })}>
+      <div className={cn("loader", { 'exit-animation': isExiting })}>
         <svg height="0" width="0" viewBox="0 0 64 64" className="absolute">
           <defs className="s-xJBuHA073rTt" xmlns="http://www.w3.org/2000/svg">
             <linearGradient className="s-xJBuHA073rTt" gradientUnits="userSpaceOnUse" y2="2" x2="0" y1="62" x1="0" id="b">
@@ -95,7 +95,7 @@ const MoodyOLoader = ({ isExiting }: { isExiting: boolean }) => {
         </svg>
         {/* d */}
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="48" width="48" className="inline-block">
-            <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="10" stroke="url(#d)" d="M 32 32 m 0, -27 a 27,27 0 1,1 0,54 a 27,27 0 1,1 0,-54 M 56 54 V 10" className="draw" pathLength="360"></path>
+            <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="10" stroke="url(#d)" d="M 54 54 V 10 L 54 32 C 54 45.8 43.8 54 32 54 C 20.2 54 10 43.8 10 32 C 10 20.2 20.2 10 32 10 C 43.8 10 54 20.2 54 32" className="draw" pathLength="360"></path>
         </svg>
         {/* Y */}
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="48" width="48" className="inline-block">
@@ -672,6 +672,7 @@ export default function Home() {
     
 
     
+
 
 
 
